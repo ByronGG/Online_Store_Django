47c0a293676d96d8f5073acfc45266e3d1c0855e
ByronGG <anrushgg@gmail.com>
2025-01-28 22:13:52 -0600

    Remove commented-out signIn function code for cleaner view logic

--- tasks/views.py
+++ tasks/views.py
@@ -111,31 +111,6 @@ def signOut(request):
     return redirect("home")
 
 
-"""
-def signIn(request):
-    if request.method == "GET":
-        return render(request, "signin.html", {"form": AuthenticationForm()})
-    else:
-        user = authenticate(
-            request,
-            username=request.POST["username"],
-            password=request.POST["password"],
-        )
-        if user is None:
-            return render(
-                request,
-                "signin.html",
-                {
-                    "form": AuthenticationForm(),
-                    "error": "Username or password is incorrect",
-                },
-            )
-        else:
-            login(request, user)
-            return redirect("home")
-"""
-
-
 def signIn(request):
     if request.method == "GET":
         return render(request, "signin.html", {"form": AuthenticationForm()})
