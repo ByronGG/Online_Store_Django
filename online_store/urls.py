ea58c03f778966a440e94ce3b421d9c1f3291b87
ByronGG <anrushgg@gmail.com>
2025-01-28 22:59:30 -0600

    Add media settings for user-uploaded images and update URL configuration

--- online_store/urls.py
+++ online_store/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from tasks import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -33,3 +35,6 @@ urlpatterns = [
     path("checkout/", views.checkout, name="checkout"),
     path("profile/", views.profile, name="profile"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- tasks/models.py
+++ tasks/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True) # Las Imagenes se guardará en media/products
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
